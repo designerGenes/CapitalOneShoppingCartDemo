4fc4bc329a9020aec5331f9b4468495d42456f1d
RituIosDev <ritu.dcsa@gmail.com>
2022-05-02 15:10:15 -0600

    Added Table Cell

--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/CartList/CartListTableViewCell.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/CartList/CartListTableViewCell.swift
@@ -9,5 +9,65 @@ import Foundation
 import UIKit
 
 class CartListTableViewCell: UITableViewCell {
+    private let productNameLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .black
+        lbl.font = UIFont.boldSystemFont(ofSize: 16)
+        lbl.textAlignment = .left
+        return lbl
+    }()
     
+    
+    private let productDescriptionLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .darkGray
+        lbl.font = UIFont.systemFont(ofSize: 12)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+    
+    private let productPriceLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .darkGray
+        lbl.font = UIFont.systemFont(ofSize: 16)
+        lbl.textAlignment = .right
+        return lbl
+    }()
+
+    
+    private let productImage : UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFit
+        imgView.clipsToBounds = true
+        return imgView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+       
+        addSubview(productImage)
+        addSubview(productNameLabel)
+        addSubview(productDescriptionLabel)
+        addSubview(productPriceLabel)
+
+        productImage.contentMode = .scaleAspectFit
+        productImage.clipsToBounds =  true
+        
+        productImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 0, width: 70, height: 0, enableInsets: true)
+        productNameLabel.anchor(top: topAnchor, left: productImage.rightAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 0, enableInsets: false)
+        productDescriptionLabel.anchor(top: productNameLabel.bottomAnchor, left: productImage.rightAnchor, bottom: bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 20, paddingRight: 0, width: frame.size.width - 120, height: 0, enableInsets: false)
+        
+        productPriceLabel.anchor(top: productDescriptionLabel.topAnchor, left: productDescriptionLabel.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 5, paddingBottom: 0, paddingRight: 10, width: 100, height: 50, enableInsets: false)
+
+        productNameLabel.text  = "iPhone" //TMP
+        productDescriptionLabel.text = "With Apple Trade In, you can get credit toward a new iPhone when you trade in an eligible smartphone.1 It’s good for you and the planet."
+        productPriceLabel.text  = "$ 10,000" //TMP
+        productImage.image = UIImage(named: "thumbnail") //TMP
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
