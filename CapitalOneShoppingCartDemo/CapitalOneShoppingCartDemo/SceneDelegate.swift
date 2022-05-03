744a27a41a2eb3e8a2b1c80e800dbbe45b1e3722
RituIosDev <ritu.dcsa@gmail.com>
2022-05-02 18:10:56 -0600

    Update with Entry Point

--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/SceneDelegate.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let productListVC = CartListViewController()
+        let productListVC = ProductListViewController()
         window?.rootViewController = productListVC
         window?.makeKeyAndVisible()
     }
