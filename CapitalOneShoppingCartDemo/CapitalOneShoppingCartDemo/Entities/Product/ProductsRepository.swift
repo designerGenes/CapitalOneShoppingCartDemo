09096db79f196fc23dc9d5acd497c89e2ca232e0
Jaden Nation <jnationdesignerjeans@gmail.com>
2022-05-03 14:00:00 -0500

    update repository to save array of Products

--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Entities/Product/ProductsRepository.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Entities/Product/ProductsRepository.swift
@@ -9,4 +9,21 @@ import Foundation
 
 class ProductsRepository: NSObject, Repository {
     typealias T = Product
+    static var storageKey: String { String(describing: ProductsRepository.self) }
+    
+    func get(id: String) -> Product? {
+        getAll().filter({$0.id == id}).first
+    }
+    
+    
+    func set(record: Product) {
+        var products = getAll().filter({$0.id != record.id})
+        products.append(record)
+        set(records: products)
+    }
+    
+    func set(records: [Product]) {
+        UserDefaults.standard.set(records, forKey: ProductsRepository.storageKey)
+    }
+
 }
--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Repository.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Repository.swift
@@ -8,40 +8,45 @@
 import Foundation
 
 protocol Repository {
-    associatedtype T: DataModel
-    
+    associatedtype T
+    static var storageKey: String { get }
     func getAll() -> [T]
-    func get(recordId: String) -> T?
-    func set(record: T) -> Bool
+    func get(id: String) -> T?
+    func set(record: T)
 }
 
 extension Repository {
-    var archiveID: String {
-        "\(String(describing: self))_id"
-    }
-    var createdIds: [String] {
-        let createdIds = UserDefaults.standard.array(forKey: self.archiveID) as? [String]
-        return createdIds ?? [String]()
-    }
-    
-    func set(record: T) -> Bool {
-        guard let id = record.id else { return false }
-        UserDefaults.standard.set(record, forKey: id)
-        return true
-    }
-    
-    func get(recordId: String) -> T? {
-        UserDefaults.standard.object(forKey: recordId) as? T
-    }
-    
     func getAll() -> [T] {
-        var allRecords = [T]()
-        for createdId in self.createdIds {
-            guard let retrievedRecord = self.get(recordId: createdId) else {
-                continue
-            }
-            allRecords.append(retrievedRecord)
-        }
-        return allRecords
+        return UserDefaults.standard.array(forKey: Self.storageKey) as? [T] ?? []
     }
 }
+
+//extension Repository {
+//    var archiveID: String {
+//        "\(String(describing: self))_id"
+//    }
+//    var createdIds: [String] {
+//        let createdIds = UserDefaults.standard.array(forKey: self.archiveID) as? [String]
+//        return createdIds ?? [String]()
+//    }
+//    
+//    func set(record: T, id: String) -> Bool {
+//        UserDefaults.standard.set(record, forKey: id)
+//        return true
+//    }
+//    
+//    func get(id: String) -> T? {
+//        UserDefaults.standard.object(forKey: id) as? T
+//    }
+//    
+//    func getAll() -> [T] {
+//        var allRecords = [T]()
+//        for createdId in self.createdIds {
+//            guard let retrievedRecord = self.get(id: createdId) else {
+//                continue
+//            }
+//            allRecords.append(retrievedRecord)
+//        }
+//        return allRecords
+//    }
+//}
