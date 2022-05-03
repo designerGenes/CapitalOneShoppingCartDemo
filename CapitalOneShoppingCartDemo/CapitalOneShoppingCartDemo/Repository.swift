251b893da2d07fa07688a34535c60612bf901906
Jaden Nation <jnationdesignerjeans@gmail.com>
2022-05-03 16:59:08 -0500

    remove TMP code

--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Repository.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Repository.swift
@@ -20,33 +20,3 @@ extension Repository {
         return UserDefaults.standard.array(forKey: Self.storageKey) as? [T] ?? []
     }
 }
-
-//extension Repository {
-//    var archiveID: String {
-//        "\(String(describing: self))_id"
-//    }
-//    var createdIds: [String] {
-//        let createdIds = UserDefaults.standard.array(forKey: self.archiveID) as? [String]
-//        return createdIds ?? [String]()
-//    }
-//    
-//    func set(record: T, id: String) -> Bool {
-//        UserDefaults.standard.set(record, forKey: id)
-//        return true
-//    }
-//    
-//    func get(id: String) -> T? {
-//        UserDefaults.standard.object(forKey: id) as? T
-//    }
-//    
-//    func getAll() -> [T] {
-//        var allRecords = [T]()
-//        for createdId in self.createdIds {
-//            guard let retrievedRecord = self.get(id: createdId) else {
-//                continue
-//            }
-//            allRecords.append(retrievedRecord)
-//        }
-//        return allRecords
-//    }
-//}
