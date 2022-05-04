85d45699a57a965680a627abb2f3ea9b23c48c4c
Jaden Nation <jnationdesignerjeans@gmail.com>
2022-05-04 13:31:58 -0500

    add methods for retrieving Cart and updating Cart items

--- CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Entities/Cart/Cart.swift
+++ CapitalOneShoppingCartDemo/CapitalOneShoppingCartDemo/Entities/Cart/Cart.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
-class Cart: DataModel {
+class Cart: NSObject {
+    var productIds = [String]()
+    
+    func addProduct(id: String) {
+        guard !productIds.contains(id) else {
+            return
+        }
+        productIds.append(id)
+        CartsRepository().set(record: self)
+    }
+    
+    func removeProduct(id: String) {
+        productIds = productIds.filter({$0 != id})
+        CartsRepository().set(record: self)
+    }
+    
     
 }
